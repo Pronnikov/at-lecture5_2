82573d72a4c96d84d42fb0f523b5dc416e95d6f8
smakarova <svetlana.makarova.1992@list.ru>
2022-03-15 14:03:01 +0300

    laba4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,61 +1,26 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import page.StartPage;
 
-import javax.swing.*;
 import java.time.Duration;
 
 public class Main {
 
     public static void main(String[] args) {
 
-            //System.setProperty("webdriver.chrome.driver","src/chromedriver.exe");
-           WebDriver webDriver = new ChromeDriver();
-           webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-           WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(7));
-           //webDriver.get("https://demoqa.com/buttons");
-           webDriver.get("https://demoqa.com/alerts");
-           // WebElement doubleClick = webDriver.findElement(By.id("doubleClickBtn"));
-           // WebElement rightClick = webDriver.findElement(By.id("rightClickBtn"));
-           // WebElement click = webDriver.findElement(By.xpath("//button[text()='Click Me']"));
-              WebElement alert = webDriver.findElement(By.id("alertButton"));
-              WebElement alertTimer = webDriver.findElement(By.id("timerAlertButton"));
-              WebElement confirmButton = webDriver.findElement(By.id("confirmButton"));
-          //  new Actions(webDriver)
-        //           .doubleClick(doubleClick)
-        //           .contextClick(rightClick)
-        //           .click(click)
-        //           .build()
-        //           .perform();
-       
-        alert.click();
-        webDriver.switchTo().alert().accept();
-        alertTimer.click();
-        webDriverWait.until(ExpectedConditions.alertIsPresent())
-                .accept();
-        confirmButton.click();
-        webDriver.switchTo().alert().dismiss();
-        boolean isHaveText = !webDriver.findElements(By.xpath("//span[contains(.,'Cancel')]")).isEmpty();
-        // boolean isHaveDoubleClickText = !webDriver.findElements(By.xpath("//p[text()='You have done a double click']")).isEmpty();
-       // boolean isHaveRightClickText = !webDriver.findElements(By.xpath("//p[text()='You have done a right click']")).isEmpty();
-       // boolean isHaveClickmeClickText = !webDriver.findElements(By.xpath("//p[text()='You have done a dynamic click']")).isEmpty();
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        webDriver.get("https://demoqa.com/buttons");
+
+        StartPage startPage = new StartPage(webDriver);
+        startPage.clickButton();
+        startPage.getText();
+
         webDriver.quit();
 
 
-
-       // if (isHaveDoubleClickText && isHaveRightClickText && isHaveClickmeClickText) {
-            if (isHaveText){
-        System.out.println("Тест пройден успешно");
-        } else{
-            System.out.println("Тест не пройден");
-        }
-
-        }
-
     }
+
+}
 
---
+++ src/main/java/page/StartPage.java
@@ -0,0 +1,46 @@
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class StartPage {
+
+    private static By doubleClick = By.id("doubleClickBtn");
+    private static By rightClick = By.id("rightClickBtn");
+    private static By click = By.xpath("//button[text()='Click Me']");
+    private static By doubleClickText = By.xpath("//p[text()='You have done a double click']");
+    private static By rightClickText = By.xpath("//p[text()='You have done a right click']");
+    private static By clickmeClickText = By.xpath("//p[text()='You have done a dynamic click']");
+    WebDriver driver;
+
+
+    public StartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public StartPage clickButton() {
+        new Actions(driver)
+                .doubleClick(driver.findElement(doubleClick))
+                .contextClick(driver.findElement(rightClick))
+                .click(driver.findElement(click))
+                .build()
+                .perform();
+        return new StartPage(driver);
+    }
+
+
+    public void getText() {
+        boolean isHaveDoubleClickText = !driver.findElements(doubleClickText).isEmpty();
+        boolean isHaveRightClickText = !driver.findElements(rightClickText).isEmpty();
+        boolean isHaveClickmeClickText = !driver.findElements(clickmeClickText).isEmpty();
+
+
+        if (isHaveDoubleClickText && isHaveRightClickText && isHaveClickmeClickText) {
+            System.out.println("Тест пройден успешно");
+        } else {
+            System.out.println("Тест не пройден");
+        }
+    }
+}
